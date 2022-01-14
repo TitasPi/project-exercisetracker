4c79ba09eee1a945b0804438d08b02ff6c800231
Titas <titas@pixylt.com>
2022-01-14 16:52:32 +0200

    done?

--- server.js
+++ server.js
@@ -1,15 +1,88 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema, UUID } = mongoose;
 
-app.use(cors())
-app.use(express.static('public'))
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+});
+const exerciseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: false }
+});
+
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', async (req, res) => {
+  const users = await User.where({});
+  res.json(users);
+})
 
+app.post('/api/users', async (req, res) => {
+  if(req.body.username === '') return res.json({error: 'You can\'t use empty username'});
+
+  const user = new User({ username: req.body.username });
+  await user.save();
+  res.json(user);
+});
+
+app.get('/api/users/:id/logs', async (req, res) => {
+  try {
+    const from = req.query.from;
+    const to = req.query.to;
+    const limit = req.query.limit;
+
+    const user = await User.findOne({ _id: req.params.id });
+  
+    let exercises = Exercise.where({ username: user.username }).select('description duration date -_id');
+    if(from !== undefined) exercises = exercises.where({date: { $gte: from}});
+    if(to !== undefined) exercises = exercises.where({date: { $lte: to}});
+    if(limit !== undefined) exercises = exercises.limit(limit);
+    exercises = await exercises.exec();
+    const exercisesList = [];
+    exercises.forEach((exercise) => {
+      exercisesList.push({
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString()
+      });
+    });
+  
+    res.json({_id: user._id, username: user.username, count: exercises.length, logs: exercisesList});
+  } catch (error) {
+    console.error(error);
+    res.json({error: error});
+  }
+});
+
+app.post('/api/users/:id/exercises', async (req, res) => {
+  try {
+    // res.json({body: req.body});
+    const user = await User.findOne({ _id: req.body[':_id'] });
+    let date = Date.parse(req.body.date);
+    if (isNaN(date)) date = Date.now();
+    const exercise = Exercise({username: user.username, description: req.body.description, duration: req.body.duration, date: date});
+    await exercise.save();
+    res.json(exercise);
+  } catch (error) {
+    res.json({error: error});
+  }
+});
 
 
 
